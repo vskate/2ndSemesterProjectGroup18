729f9e9639c5eefca03b0c0a1f888e5401ed6f83
JakeobC <jacun24@student.sdu.dk>
2025-04-01 13:57:22 +0200

    Adding MainWindow Menu

--- HeatProductionOptimizationApp/obj/Debug/net9.0/HeatProductionOptimizationApp.AssemblyInfo.cs
+++ HeatProductionOptimizationApp/obj/Debug/net9.0/HeatProductionOptimizationApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HeatProductionOptimizationApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e0e33f6f91e9cf4f92ba52788959f7dbc2877dd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6caeaaa2639b7f863abec1a61bda222336039e87")]
 [assembly: System.Reflection.AssemblyProductAttribute("HeatProductionOptimizationApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HeatProductionOptimizationApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
