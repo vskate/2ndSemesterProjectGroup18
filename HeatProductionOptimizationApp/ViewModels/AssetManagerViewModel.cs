54c3fb5a49fd6b3ad915beb639075a80f6db0c3d
2xdox1 <158808978+2xdox1@users.noreply.github.com>
2025-04-15 17:10:31 +0200

    UI now pulls data from AssetManagerViewModel and lists production units

--- HeatProductionOptimizationApp/ViewModels/AssetManagerViewModel.cs
+++ HeatProductionOptimizationApp/ViewModels/AssetManagerViewModel.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using HeatProductionOptimizationApp.Models;
 
 namespace HeatProductionOptimizationApp.ViewModels;
 
 public partial class AssetManagerViewModel : ViewModelBase
 {
-
+    public List<ProductionUnit> Units => AssetManager.ProductionUnits;
 }
