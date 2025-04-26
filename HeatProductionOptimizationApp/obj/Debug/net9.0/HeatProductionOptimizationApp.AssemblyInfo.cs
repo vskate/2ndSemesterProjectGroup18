99b9879277eb8eb337f8c0bd088d15a5897252c6
2xdox1 <158808978+2xdox1@users.noreply.github.com>
2025-04-26 16:02:19 +0200

    Checkpoint: working data load into DataRepository, ready to build AssetManager properly

--- HeatProductionOptimizationApp/obj/Debug/net9.0/HeatProductionOptimizationApp.AssemblyInfo.cs
+++ HeatProductionOptimizationApp/obj/Debug/net9.0/HeatProductionOptimizationApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HeatProductionOptimizationApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5d9a65f76767dd0c758b54e174072e737295eec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45c96454619f62d98778e4d81f8d03d06bc993cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("HeatProductionOptimizationApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HeatProductionOptimizationApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
